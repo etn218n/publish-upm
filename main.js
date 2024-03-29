cb0f7b598b0c12b98acf6269b50d9420a02bf80b
Duy <etn218n@gmail.com>
2024-03-29 19:42:19 +0200

    Test fs.chown

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const upm = require('./upm.js');
 const fs = require('fs');
-const {execSync} = require('child_process');
 
 if (process.argv.length !== 5) {
     console.log('Usage: node main.js <package-json-file>');
@@ -9,12 +8,12 @@ if (process.argv.length !== 5) {
 
 let storageDirectory = processStorageDirectory(process.argv[2]);
 let registry  = process.argv[3];
-let ownerShip = process.argv[4];
+let ownerShip = process.argv[4].split(':');
 
 if (fs.existsSync('./package.json')) {
     let manifest = require('./package.json');
     let packageDirectory = upm.publish(manifest, registry, storageDirectory);
-    execSync(`sudo chown -R ${ownerShip} ${packageDirectory}`);
+    fs.chownSync(packageDirectory, parseInt(ownerShip[0]), parseInt(ownerShip[1]));
 }
 else {
     console.log('ERROR: No package.json file found!');
