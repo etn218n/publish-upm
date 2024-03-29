60c311fe15dbf3c5004fea912445babcd4d3c3f0
Duy <etn218n@gmail.com>
2024-03-29 19:59:02 +0200

    2

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const upm = require('./upm.js');
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
+if (process.argv.length !== 4) {
     console.log('Usage: node main.js <package-json-file>');
     process.exit(-1);
 }
 
 let storageDirectory = processStorageDirectory(process.argv[2]);
 let registry  = process.argv[3];
-let ownerShip = process.argv[4].split(':');
 
 if (fs.existsSync('./package.json')) {
     let manifest = require('./package.json');
-    let packageDirectory = upm.publish(manifest, registry, storageDirectory);
-    fs.chownSync(packageDirectory, parseInt(ownerShip[0]), parseInt(ownerShip[1]));
+    upm.publish(manifest, registry, storageDirectory);
 }
 else {
     console.log('ERROR: No package.json file found!');
