9ba74334658bd6b6e625c23072d5c22675752e65
Duy <etn218n@gmail.com>
2024-03-29 22:12:08 +0200

    3

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const upm = require('./upm.js');
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
+if (process.argv.length !== 5) {
     console.log('Usage: node main.js <package-json-file>');
     process.exit(-1);
 }
 
 let storageDirectory = processStorageDirectory(process.argv[2]);
-let registry  = process.argv[3];
+let jsonFile = process.argv[3];
+let registry = process.argv[4];
 
-if (fs.existsSync('./package.json')) {
-    let manifest = require('./package.json');
+if (fs.existsSync(jsonFile)) {
+    let manifest = require(jsonFile);
     upm.publish(manifest, registry, storageDirectory);
 }
 else {
